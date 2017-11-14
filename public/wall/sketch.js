d61127df98bcffe7b69d0da6df4d24ec856a444d
Derick Ostrenko <dostrenko@gmail.com>
2017-11-14 13:47:38 -0600

    new logo and new wall

--- public/wall/sketch.js
+++ public/wall/sketch.js
@@ -16,8 +16,14 @@ function setup() {
 
 var n = 0;
 
+var h1 = 0;
+var s1 = 0;
+var l1 = 20;
+var w = 0;
+
 function draw() {
-  background(h, s, l);
+  // background(h, s, l);
+  background(h1, s1, l1);
   
   var nstr = quotes[quoteNumber].replace("—", "<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;—");
   document.getElementById("quote").innerHTML=nstr;  
@@ -29,6 +35,24 @@ function draw() {
   translate(width, 0);
   rotate(radians(n));
   image(compass, 0, 0, windowHeight*1.5, windowHeight*1.5);
+
+
+  console.log(mcount, pmcount);
+  
+    if (mcount != pmcount) {
+      w++
+      console.log("BAANNGGG", w)
+      pmcount = mcount;
+      h1 = h;
+      s1 = s;
+      l1 = l;
+    }
+  
+    if (mcount = pmcount) {
+      if (l1 > 0) { 
+        l1-=0.5; 
+      }
+    }
 
 }
 
